6a3bfa5545163392cb5a3dfe3e55b10095706306
sara <sara0mahdi@gmail.com>
2026-04-30 22:20:09 +0300

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class State {
+    public int id;
+    public Map<Character, List<State>> transitions = new HashMap<>();
+    public List<State> epsilon = new ArrayList<>();
+    public String tokenName = null;
+
+    public State(int id) {
+        this.id = id;
+    }
+
+    public void addTransition(char c, State s) {
+        transitions.computeIfAbsent(c, k -> new ArrayList<>()).add(s);
+    }
+
+    public void addEpsilon(State s) {
+        epsilon.add(s);
+    }
+}
