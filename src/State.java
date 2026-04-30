442765fa38d5f7024d14e0f936de4340f825a405
sara <sara0mahdi@gmail.com>
2026-04-30 22:33:16 +0300

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,10 +1,10 @@
 import java.util.*;
-
+//  class for representing the node
 public class State {
     public int id;
     public Map<Character, List<State>> transitions = new HashMap<>();
-    public List<State> epsilon = new ArrayList<>();
-    public String tokenName = null;
+    public List<State> epsilon = new ArrayList<>();   //epslon transition    
+    public String tokenName = null;   // to store the name of token if it was accepting state
 
     public State(int id) {
         this.id = id;
