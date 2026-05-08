2a973e7f607eb3a35f9d6df0b3072723c1c08066
Dana-Mutairi <dal795561@gmail.com>
2026-05-08 21:37:04 +0300

    Update NFA.java

--- src/NFA.java
+++ src/NFA.java
@@ -1,9 +1,10 @@
-// this class represent NFA machine
-public class NFA {
-    public State start, end;    // statr and end state
 
-    public NFA(State s, State e) {
-        start = s;
-        end = e;
-    }
+//this class represent NFA machine
+public class NFA {
+ public State start, end;    // state and end state
+
+ public NFA(State s, State e) {
+     start = s;
+     end = e;
+ }
 }
