12d53dfeb5163311257fa1bcd5f0c786b2a2f847
Dana-Mutairi <dal795561@gmail.com>
2026-05-08 21:37:41 +0300

    Update Token.java

--- src/Token.java
+++ src/Token.java
@@ -1,1 +1,29 @@
 
+
+public class Token {
+
+    public String lexeme;
+    public String type;
+    public int line;
+    public int column;
+
+    public Token(String lexeme, String type,
+                 int line, int column) {
+
+        this.lexeme = lexeme;
+        this.type = type;
+        this.line = line;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+
+        return "'" + lexeme + "'  "
+                + type
+                + "  Line "
+                + line
+                + ", col "
+                + column;
+    }
+}
